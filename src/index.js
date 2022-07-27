1dcf43d461074062bdd30d0d9d6bb91428f3020b
Exx1 <exx-3@yandex.ru>
2022-07-27 17:02:17 +0300

    1

--- src/index.js
+++ src/index.js
@@ -35,10 +35,60 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
     // write your solution here
+    let arr = [];
+    for (let i = expr.length / 10; i > 0; i--) {
+        arr.unshift(expr.slice(-10));
+        expr = expr.slice(0, -10);
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        let b = 0;
+        let count = 0;
+        while (b < arr[i].length) {
+            
+            if (arr[i][b] !== '0') {
+                arr[i] = arr[i].slice(count);
+                break;
+            }
+                count = count + 1;
+            b++;
+        }
+        
+    }
+    
+    
+
+    for (let i = 0; i < arr.length; i++) {
+        let strMorse = '';
+        let b = 1;
+        for (let b = 1; b < arr[i].length; b=b+2) {
+            if (arr[i][b] === '*') {
+                strMorse = strMorse + '**'
+            } else if (arr[i][b] === '0') {
+                strMorse = strMorse + '.'
+                
+            } else {
+                strMorse = strMorse + '-'
+            }
+        }
+        arr[i] = strMorse;
+    }
+
+    let phrase = ''
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let key in MORSE_TABLE) {
+            if (arr[i] === key) {
+                phrase = phrase + MORSE_TABLE[key];
+            }
+        }
+    }
+    return phrase;
 }
 
 module.exports = {
